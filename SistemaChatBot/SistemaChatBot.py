30bf3954824e7796d347239759b9068e70e8a5fd
hpereira1 <pereirah123@gmail.com>
2022-05-13 10:58:21 -0300

    sist bot

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -4,7 +4,7 @@ class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
         self.__empresa=nomeEmpresa
         ##verificar se a lista de bots contém apenas bots
-        self.__lista_bots=lista_bots
+        self.__lista_bots=[lista_bots]
         self.__bot = None
     
     def boas_vindas(self):
@@ -13,26 +13,29 @@ class SistemaChatBot:
 
     def mostra_menu(self):
         print('temos 2 bots disponiveis: o brabo e o goodvibes')
-        aux = int(input('digite:\n 0 para o brabo \n1 para o goodvibes: ')
-        while aux not in :
-           aux = int(input('input fora do escopo, digite 0 para o brabo ou 1 para o goodvibes: ')
-        escolhe_bot(aux)
-        pass
+        print('digite:\n 1 - Brabo \n2 - Goodvibes ')
+
         ##mostra o menu de escolha de bots
     
-    def escolhe_bot(self):
-
-        pass
-        ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
+    def escolhe_bot(self,aux):
+        aux = input()
+        while aux not in ['1', '2']:
+           aux = input('input fora do escopo, digite 0 para o brabo ou 1 para o goodvibes: ')
+       ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
 
     def mostra_comandos_bot(self):
-        pass
-        ##mostra os comandos disponíveis no bot escolhido
+        while True:
+            oper = {
+                1: apresentacao,
+                2: boas_vindas,
+                3: executa_comando,
+                4: despedida
 
+            }   
+            print('1 - apresentacao\n 2 - boas vindas\n 3 - executa comando\n 4 - despedida')
+        
     def le_envia_comando(self):
-        pass
-        ##faz a entrada de dados do usuário e executa o comando no bot ativo
-
+    
     def inicio(self):
         pass
         ##mostra mensagem de boas-vindas do sistema
