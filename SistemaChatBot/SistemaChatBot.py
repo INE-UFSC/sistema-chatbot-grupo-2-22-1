b4cae2dccca1a376ffe78e7d378581f5eb9506c2
hpereira <pereirah123@gmail.com>
2022-06-24 09:45:10 -0300

    sistema

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -1,15 +1,35 @@
 from Bots.Bot import Bot
 
+
 class SistemaChatBot:
-    def __init__(self,nomeEmpresa,lista_bots):
-        self.__empresa = nomeEmpresa
-        self.__lista_bots = []
+    def __init__(self, Empresa: str, lista_bots: list):
+        self.__empresa = Empresa
+        self.__lista_bots = lista_bots
         self.__bot = None
 
+    @property
+    def empresa(self):
+        return self.__empresa
+
+    @empresa.setter
+    def empresa(self, empresa):
+        self.__empresa = empresa
+
+    @property
+    def lista_bots(self):
+        return self.__lista_bots
+
+    @property
+    def bot(self):
+        return self.__bot
+
+    @bot.setter
+    def bot(self, bot):
+        self.__bot = bot
 
     def boas_vindas_sistema(self):
         print(f'Ola, esse é o sistema de chatbots do Grupo {self.__empresa}\n')
-        ##mostra mensagem de boas vindas do sistema
+        # mostra mensagem de boas vindas do sistema
 
     def mostra_menu(self):
         print('temos esses bots disponiveis:')
@@ -19,14 +39,13 @@ class SistemaChatBot:
     def escolhe_bot(self):
         aux = input('digite o codigo do bot desejado: ')
         while aux not in enumerate(self.__lista_bots):
-           aux = input('input fora do escopo')
-       ##faz a entrada de dadosf do usuário e atribui o objeto ao atributo __bot 
+            aux = input('input fora do escopo')
+        # faz a entrada de dadosf do usuário e atribui o objeto ao atributo __bot
 
     def inicio(self):
-        pass
-        ##mostra mensagem de boas-vindas do sistema
-        ##mostra o menu ao usuário
-        ##escolha do bot      
-        ##mostra mensagens de boas-vindas do bot escolhido
-        ##entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
-        ##ao sair mostrar a mensagem de despedida do bot
+        self.boas_vindas_sistema()
+        self.mostra_menu()
+        self.escolhe_bot()
+        # mostra mensagens de boas-vindas do bot escolhido
+        # entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
+        # ao sair mostrar a mensagem de despedida do bot
